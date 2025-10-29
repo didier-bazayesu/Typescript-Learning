60dcd14f123f2e852931f2e9936deddbf8e40afd
didier-bazayesu <didierbazayesu@gmail.com>
2025-10-29 21:13:33 +0200

    feat:learning type in typescript

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,36 @@ function calculateTax(income:number , taxyear:number):number{
   if(taxyear < 2022) return income * 1.2;
   income+= income  * 1.3 ;
   return income;
+
+
+
+  // 1. literal type 
+
+  let tax : "up"| "down" | "left" | "right";
+
+  tax = "up";
+
+  // 2. union type 
+
+  //the variable might have multiple type either string type or other type 
+
+  let string : string | number | boolean= true
+
+  //3 . custome type 
+
+  //user defined type 
+
+  type didier = {
+    name : string ;
+    age : number
+    isValid : Boolean
+  }
+
+  let myObject:didier = {
+
+    name : "didier",
+    age : 23,
+    isValid : true,
+    
+  }
+}
