918e232f501b4e2813461176ae729de3813952be
didier-bazayesu <didierbazayesu@gmail.com>
2025-10-30 21:12:41 +0200

    feat: array explicit

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,47 @@ function checkType(str:number|string|boolean){
 }
 
 console.log(checkType('didier'))
+
+
+//on array 
+
+let array: [String, number] = ['dider', 3];
+
+array.push(5);
+console.log(array);
+
+//creating default array
+
+let nameContainer = [2, 'didier',false]
+nameContainer.push(true)
+console.log(nameContainer);
+
+
+// object 
+
+type shape = {
+  name : string,
+  age : 30,
+  greet : ()=> number,
+
+}
+
+let house = {
+
+  room : 3,
+  color : 'white',
+  isCold : true
+}
+
+let houses = {
+  room:3,
+
+  color: 'red',
+  isCold : false
+}
+
+//explicit on array
+
+let numberArray : number[];
+numberArray = []
+numberArray.push(34646);
