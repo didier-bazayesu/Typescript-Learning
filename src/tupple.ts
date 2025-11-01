dea3c562ee869d1b529ba501771dfdc0a6e7c8b9
didier-bazayesu <didierbazayesu@gmail.com>
2025-11-01 23:37:47 +0200

    feat: adding tupple example

--- src/tupple.ts
+++ src/tupple.ts
@@ -1,3 +1,19 @@
-let array:[string,number];
+// let array:[string,number];
 
-array = ['didier',23,]
+// array = ['didier',23,]
+
+
+
+function changeInput(str:number|string){
+
+   if(typeof str == 'string'){
+   return  str.length
+   }else
+   {
+
+    return str+=46;
+   }
+
+}
+
+console.log(changeInput(6666));
