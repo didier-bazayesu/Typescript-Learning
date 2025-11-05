e5ded0b5e55e8a5d3ab0a0f6ba2d0ab5feda014a
didier-bazayesu <didierbazayesu@gmail.com>
2025-11-05 20:48:15 +0200

    feat: learning object

--- dist/index.js
+++ dist/index.js
@@ -1,18 +1,5 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-function checkType(str) {
-    if (typeof str === 'string')
-        return str.toLocaleUpperCase();
-    return str = true;
-}
-console.log(checkType('didier'));
-let array = ['dider', 3];
-array.push(5);
-console.log(array);
-let nameContainer = [2, 'didier', false];
-nameContainer.push(true);
-console.log(nameContainer);
-let numberArray;
-numberArray = [];
-numberArray.push(34646);
+const anchor = document.querySelector("a");
+console.log(anchor);
 //# sourceMappingURL=index.js.map
