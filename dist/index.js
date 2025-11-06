0202e3d274ff28555c2f6e4894c3b3d446893db4
didier-bazayesu <didierbazayesu@gmail.com>
2025-11-06 22:03:11 +0200

    feat: type checking pratical

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,16 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const anchor = document.querySelector("a");
-console.log(anchor);
+function checkParameter(value) {
+    return value;
+}
+;
+console.log(checkParameter(45));
+function checkUnion(param) {
+    console.log(param);
+}
+console.log(checkUnion('didier'));
+let number;
+number = 'didier';
+number = 10;
+console.log(number);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,43 @@
-const anchor = document.querySelector("a");
+// const anchor = document.querySelector("a");
 
-console.log(anchor);
+// console.log(anchor);
+
+
+// let arr = [2,3,4,6,4,6,7,0];
+// console.log(arr);
+
+
+
+//Learning type variable 
+
+function checkParameter <t>(value:t):t{
+
+     return value;
+};
+
+console.log(checkParameter(45));
+
+
+//learning union check
+
+function checkUnion (param:unknown){
+console.log(param);
+
+}
+
+console.log(checkUnion('didier'));
+
+
+
+//any type 
+
+let number : unknown;
+
+number = 'didier';
+number = 10;
+
+console.log(number);
+
+
+//
+
