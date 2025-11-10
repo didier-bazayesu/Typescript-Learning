025c21c63ebb2bc00d671cf0d0679f4dcab19d11
didier-bazayesu <didierbazayesu@gmail.com>
2025-11-10 19:17:59 +0200

    feat: default parameter

--- src/index.ts
+++ src/index.ts
@@ -10,34 +10,62 @@
 
 //Learning type variable 
 
-function checkParameter <t>(value:t):t{
+// function checkParameter <t>(value:t):t{
 
-     return value;
-};
+//      return value;
+// };
 
-console.log(checkParameter(45));
+// console.log(checkParameter(45));
 
 
-//learning union check
+// //learning union check
 
-function checkUnion (param:unknown){
-console.log(param);
+// function checkUnion (param:unknown){
+// console.log(param);
 
+// }
+
+// console.log(checkUnion('didier'));
+
+
+
+// //any type 
+
+// let number : unknown;
+
+// number = 'didier';
+// number = 10;
+
+// console.log(number);
+
+
+// //checking property in object and return something 
+
+// interface mow  {
+//   name : 'didier';
+//  greet: ()=> void;
+// }
+
+//  interface greet{
+//     name : 'rukundo';
+//     greet: ()=> void;
+//  }
+
+
+
+
+//learning type annotation with code academy
+
+
+function proclaim(status = 'not ready...', repeat = 1) {
+   for (let i = 0; i < repeat; i += 1) {
+      console.log(`I'm ${status}`);
+   }
 }
 
-console.log(checkUnion('didier'));
+proclaim();
+proclaim('ready?');
+proclaim('ready!', 3);
 
 
-
-//any type 
-
-let number : unknown;
-
-number = 'didier';
-number = 10;
-
-console.log(number);
-
-
-//
 
