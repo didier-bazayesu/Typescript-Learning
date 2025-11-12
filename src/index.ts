53fa7cb30e656b505555c9b718f17c953a6f90c7
didier-bazayesu <didierbazayesu@gmail.com>
2025-11-12 19:48:28 +0200

    feat: learning tupple and enum

--- src/index.ts
+++ src/index.ts
@@ -80,3 +80,56 @@ function str(name: string) {
 };
 console.log(sayHi('didier', str))
 
+
+
+
+//learning type rest parameter
+
+
+function addPower(p: number, ...numsToAdd: number[]): number {
+   let answer = 0;
+   for (let i = 0; i < numsToAdd.length; i++) {
+      let array = numsToAdd
+      // answer += array[i] ** p;
+   }
+   return answer;
+}
+
+console.log(addPower(8, 4, 5, 6))
+
+
+
+//spread operator type checking with using tuppples
+
+
+
+function performDanceMove(moveName: string, moveReps: number, hasFlair: boolean): void {
+
+   console.log(`I do the ${moveName} ${moveReps} times !`);
+   if (hasFlair) {
+      console.log('I do it with flair!');
+   }
+}
+
+
+type nameArrias = [string, number, boolean];
+let danceMoves: nameArrias[] = [
+   ['chicken beak', 4, false],
+   ['wing flap', 4, false],
+   ['tail feather shake', 4, false],
+   ['clap', 4, false],
+   ['chicken beak', 4, true],
+   ['wing flap', 4, true],
+   ['tail feather shake', 4, true],
+   ['clap', 4, true],
+];
+
+for (let elem of danceMoves) {
+   performDanceMove(elem[0], elem[1], elem[2]);
+}
+
+
+
+//understanding enum
+
+
